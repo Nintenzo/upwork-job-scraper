033f7b4a958816a3407081deb242624c5abe2c0d
Nintenzo <m.ahmed.hamdy987@gmail.com>
2025-04-09 14:56:44 +0200

    origin

--- upwork.py
+++ upwork.py
@@ -9,6 +9,7 @@ import os
 
 load_dotenv()
 
+jobset = set()
 # Edit the following to add your own Telegram bot token and chat ID in the .env file
 TOKEN = os.getenv("TOKEN") # TOKEN=<YourTelegramBotToken>
 CHAT_ID = os.getenv("CHAT_ID") # CHAT_ID=<YourChatID>
@@ -16,7 +17,7 @@ CHAT_ID = os.getenv("CHAT_ID") # CHAT_ID=<YourChatID>
 scraper = cloudscraper.create_scraper()
 
 # Edit the URL to specify the job search query on Upwork
-url = "https://www.upwork.com/nx/search/jobs/?page=1&per_page=20&q=%28automation%20OR%20python%20OR%20automate%20OR%20bot%20OR%20spreadsheet%20OR%20scrap%29"
+url = "https://www.upwork.com/nx/search/jobs/"
 
 # Ensure the "warp-cli" command is available on your system, or remove if not using Warp VPN
 def run_warp():
@@ -31,11 +32,11 @@ def telegram(message=None):
 	try:
 		if message:
 			for x in message:
-				print(x)
-				part1 = f"Title: {x}\nProposal: {message[x][2]}\nPrice: {message[x][3]}\n"
-				part2 = message[x][0]
-				part3 = message[x][1]
-				combined_message = f"{part1}\n{part2}\n{part3}"
+				# title,clean_text0,link1,proposal2,price_text3,price4,experience_text5,experience6,proposal7
+				part1 = f"Title: {x}\nProposal: {message[x][7].strip()}\n{message[x][3].strip()}: {message[x][4].strip()}\n{message[x][5].strip()}: {message[x][6].strip()}"
+				part2 = f"Link: {message[x][1]}"
+				part3 = message[x][0]  
+				combined_message = f"{part1}\n\n{part3}\n\n{part2}"
 
 				chunks = [combined_message[i:i+4096] for i in range(0, len(combined_message), 4096)]
 
@@ -52,18 +53,6 @@ def telegram(message=None):
 					else:
 						print(f"Failed to send chunk: {response.status_code}")
 
-				separator = "\n==============================================================================================================================================================================\n"
-				url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-				data = {
-					"chat_id": CHAT_ID,
-					"text": separator
-				}
-				response = requests.post(url=url, data=data)
-				if response.status_code == 200:
-					print("Separator sent.")
-				else:
-					print(f"Failed to send separator: {response.status_code}")
-
 	except Exception as e:
 		print(f"Error occurred: {e}")
 		return
@@ -76,42 +65,70 @@ def last():
     old_job = soup.find('h2',class_='h5 mb-0 mr-2 job-tile-title').text
     print(old_job)
 
-def main():
-    global old_job
-    while True:
-        jobs = {}
-        response = scraper.get(url)
-        html = response.text
-        soup = BeautifulSoup(html, "html.parser")
-        job = soup.find_all('h2',class_='h5 mb-0 mr-2 job-tile-title')
-        last_job = soup.find('h2',class_='h5 mb-0 mr-2 job-tile-title').text
-        for x in job:
-            a_tag = x.find('a')
-            if a_tag:
-                try:
-                    title = a_tag.text.strip()
-                    if title == old_job:
-                        old_job = last_job
-                        time.sleep(60)
+def scrapy(soup):
+    global old_job,jobset
+    jobs = {}
+    job = soup.find_all('h2',class_='h5 mb-0 mr-2 job-tile-title')
+    last_job = soup.find('h2',class_='h5 mb-0 mr-2 job-tile-title').text
+    for x in job:
+        a_tag = x.find('a')
+        if a_tag:
+            try:
+                title = a_tag.text.strip()
+                if title == old_job:
+                    old_job = last_job
+                    time.sleep(120)
+                    break
+                link = 'https://upwork.com'+a_tag['href']
+                response = scraper.get(link)
+                html = response.text
+                soup = BeautifulSoup(html, "html.parser")
+                text = soup.find('div',class_='break mt-2').text
+                clean_text = re.sub(r'\s+', ' ', text).strip()
+                price = "Not specified"
+                experience = "Not specified"
+                experience_text = "Not specified"
+                price_elements = soup.find_all(class_='description')
+                for elm in price_elements:
+                    pricecheck = elm.find_previous_sibling().get_text(strip=True)
+                    if pricecheck[0] == "$":
+                        price_text = elm.text
+                        price = pricecheck
                         break
-                    link = 'https://upwork.com'+a_tag['href']
-                    response = scraper.get(link)
-                    html = response.text
-                    soup = BeautifulSoup(html, "html.parser")
-                    text = soup.find('div',class_='break mt-2').text
-                    clean_text = re.sub(r'\s+', ' ', text).strip()
-                    priceelement = soup.find_all(class_='description')
-                    price = priceelement[0].find_previous_sibling()
-                    price = price.get_text(strip=True)
-                    if price[0] != "$":
-                        price = priceelement[3].find_previous_sibling()
-                        price = price.get_text(strip=True)
-                        if price == "Ongoing project" or "Complex project":
-                              price = "Not Sure"
-                    proposal = soup.find(class_='value').text
-                    jobs[title] = clean_text,link,proposal,price
-                except Exception as e:
-                    print(e)
-        telegram(jobs)
+                    else:
+                        price = "Price not set"
+                for elm in price_elements:
+                    experiencecheck = elm.get_text(strip=True)
+                    experience = elm.find_previous_sibling().get_text(strip=True)
+                    if experiencecheck == "Experience Level":
+                        experience_text = elm.text
+                        experience = experience
+                        break
+                    else:
+                        experience = "Experience not set"
+                proposal_element = soup.find(class_='value')
+                proposal = proposal_element.text if proposal_element else "Not specified"
+                if link in jobset: 
+                    continue
+                else:
+                    jobset.add(link)
+                    jobs[title] = clean_text,link,proposal,price_text,price,experience_text,experience,proposal
+            except Exception:
+                pass
+    return jobs 
+            
+def main():
+    global jobset
+    try:
+        while True:
+            if len(jobset) >= 100:
+                 jobset.clear()
+            response = scraper.get(url)
+            html = response.text
+            soup = BeautifulSoup(html, "html.parser")
+            data = scrapy(soup)
+            telegram(data)
+    except Exception as e:
+        print(e)
 last()
 main()
